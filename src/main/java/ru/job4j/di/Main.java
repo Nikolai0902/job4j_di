e8424727b183fd8a5d4d4c3eebcdbcc53fc0550f
Nikolai0902 <kbus94@yandex.ru>
2023-10-23 23:19:30 +0300

    2. Сканирование проекта [#1070]

--- src/main/java/ru/job4j/di/Main.java
+++ src/main/java/ru/job4j/di/Main.java
@@ -1,14 +1,15 @@
 package ru.job4j.di;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        Context context = new Context();
-        context.reg(Store.class);
-        context.reg(ConsoleInput.class);
-        context.reg(StartUI.class);
-        StartUI ui = context.get(StartUI.class);
-        ui.add("Petr Arsentev");
-        ui.add("Ivan ivanov");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.scan("ru.job4j.di");
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add();
+        ui.add();
         ui.print();
     }
 }
--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -1,15 +1,20 @@
 package ru.job4j.di;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class StartUI {
 
     private Store store;
+    private ConsoleInput consoleInput;
 
-    public StartUI(Store store) {
+    public StartUI(Store store, ConsoleInput consoleInput) {
         this.store = store;
+        this.consoleInput = consoleInput;
     }
 
-    public void add(String value) {
-        store.add(value);
+    public void add() {
+        store.add(consoleInput.askStr("Enter name:" + System.lineSeparator()));
     }
 
     public void print() {
